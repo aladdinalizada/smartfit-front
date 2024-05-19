bb7aad3a423814df43e677022f061eb3251a4744
Aladdin Alizada <tu7c5zneb@code.edu.az>
2024-05-20 01:27:05 +0400

    config react helmet

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,17 @@ import Innovation from "./components/Innovation";
 import TrustedPeople from "./components/TrustedPeople";
 import StartDemo from "./components/StartDemo";
 import FAQ from "./components/FAQ";
-
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <main className="main">
+      <Helmet>
+        <title>SmartFit </title>
+        <meta
+          name="description"
+          content="SmartFit is a smart fitness app that helps you to stay fit and healthy. It provides you with a personalized workout plan and diet plan."
+        />
+      </Helmet>
       <Hero />
       <TryItNow />
       <Props />
